01bc0e286c54e1c2412d9ea3c1127ce99823ba5f
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2023-12-01 21:21:01 +0100

    fix: fixes ts bug

--- app/page.tsx
+++ app/page.tsx
@@ -60,10 +60,12 @@ export default function Page() {
           renderTooltip={({ tooltipData, colorScale }) => {
             return (
               <div className="z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
-                <div style={{ color: colorScale('dataPoint') }}>{tooltipData?.datumByKey['dataPoint'].key}</div>
-                {accessors.xAccessor(tooltipData?.datumByKey['dataPoint'].datum)}
+                <div style={{ color: colorScale ? colorScale('dataPoint') : '#ff6969' }}>
+                  {tooltipData?.datumByKey['dataPoint'].key}
+                </div>
+                {accessors.xAccessor(tooltipData?.datumByKey['dataPoint'].datum as IDataPoint)}
                 {', '}
-                {accessors.yAccessor(tooltipData?.datumByKey['dataPoint'].datum)}
+                {accessors.yAccessor(tooltipData?.datumByKey['dataPoint'].datum as IDataPoint)}
               </div>
             );
           }}
