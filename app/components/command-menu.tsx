55f25f0d63ed40e1e3ff1539965452ee3e567c43
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2023-12-02 23:59:20 +0100

    chore: tested features, added orm etc

---
+++ app/components/command-menu.tsx
@@ -0,0 +1,85 @@
+'use client';
+import {
+  CalculatorIcon,
+  CalendarDaysIcon,
+  CreditCardIcon,
+  FaceSmileIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { StopCircle } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from './ui/command';
+
+export function CommandMenu() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  return (
+    <>
+      <p className="text-sm text-muted-foreground">
+        Press{' '}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>J
+        </kbd>
+      </p>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <CalendarDaysIcon className="mr-2 h-4 w-4" />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <FaceSmileIcon className="mr-2 h-4 w-4" />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <CalculatorIcon className="mr-2 h-4 w-4" />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <UserCircleIcon className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCardIcon className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <StopCircle className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
--- app/components/navigation/navigation.tsx
+++ app/components/navigation/navigation.tsx
@@ -1,6 +1,8 @@
+'use client';
 import logo from '@/public/velocity-logo.svg';
 import { Bars3Icon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
+import Link from 'next/link';
 import { ModeToggle } from '../theme-provider/theme-toggle';
 import { Typography } from '../typography';
 import { Sheet, SheetTrigger } from '../ui/sheet';
@@ -8,9 +10,9 @@ import { MobileSheet } from './mobile-sheet';
 import { INavigationRoute, NavigaionItems } from './navigation-items';
 
 const navigation: INavigationRoute[] = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Swimmer', href: '#' },
+  { name: 'Comparison', href: '#' },
+  { name: 'About', href: '#' },
 ];
 
 export function Navigation() {
@@ -18,15 +20,14 @@ export function Navigation() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <Sheet>
         <nav className="mx-auto flex items-center justify-between px-6 py-4" aria-label="Global">
-          <div className="flex flex-row items-center gap-x-4">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image priority className="h-8 w-auto text-fill text-foreground" src={logo} alt="Velocity logo" />
-            </a>
+          <Link href="/" className="-m-1.5 p-1.5 flex flex-row items-center gap-x-4">
+            <span className="sr-only">Your Company</span>
+            <Image priority className="h-8 w-auto text-fill text-foreground" src={logo} alt="Velocity logo" />
             <Typography variant="large" component="span" className="hidden lg:block">
               velocity
             </Typography>
-          </div>
+          </Link>
+
           <div className="flex lg:hidden">
             <SheetTrigger asChild>
               <button
@@ -38,11 +39,9 @@ export function Navigation() {
               </button>
             </SheetTrigger>
           </div>
-          <div className="flex flex-end items-center gap-x-8">
+          <div className="hidden lg:flex flex-end items-center align-middle  gap-x-8">
             <NavigaionItems.Desktop navigationRoutes={navigation} />
-            <div className="hidden lg:block">
-              <ModeToggle />
-            </div>
+            <ModeToggle />
           </div>
         </nav>
         <MobileSheet navigationRoutes={navigation} />
