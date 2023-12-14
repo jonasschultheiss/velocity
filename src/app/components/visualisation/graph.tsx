7e44af3523a28a140d985ed3f472b8c1ea152834
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2023-12-14 22:05:14 +0100

    fix: fixes build

--- src/app/components/visualisation/graph.tsx
+++ src/app/components/visualisation/graph.tsx
@@ -64,11 +64,11 @@ export default function Graph() {
               return (
                 <div className="z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
                   <div style={{ color: colorScale ? colorScale('dataPoint') : '#ff6969' }}>
-                    {tooltipData?.datumByKey['dataPoint'].key}
+                    {tooltipData?.datumByKey['dataPoint']?.key}
                   </div>
-                  {accessors.xAccessor(tooltipData?.datumByKey['dataPoint'].datum as IDataPoint)}
+                  {accessors.xAccessor(tooltipData?.datumByKey['dataPoint']?.datum as IDataPoint)}
                   {', '}
-                  {accessors.yAccessor(tooltipData?.datumByKey['dataPoint'].datum as IDataPoint)}
+                  {accessors.yAccessor(tooltipData?.datumByKey['dataPoint']?.datum as IDataPoint)}
                 </div>
               );
             }}
